183de292f4400987e8c9052deb54d5453c47232e
moonyellow <moonyellow@126.com>
2017-08-08 18:10:25 +0800

    pc项目开始

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+import App from './App.vue'
 import router from './router'
+
+Vue.use(ElementUI)
+
 Vue.filter("color", function (value) {
   if (!value && value != 0) {
     return ""
@@ -34,6 +39,7 @@ Vue.config.productionTip = false;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+    render: h => h(App),
   router,
   template: '<App/>',
   components: { App }
